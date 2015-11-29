4d25616494fffa984969a97b6d96746c313f481f
Suguru Motegi <hollow.03.03.bleach@gmail.com>
2015-11-28 16:40:29 -0800

    feat(calculator): add calculator

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+
+gulp.task('server', () => {
+  gulp.src('')
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+      fallback: './public/index.html'
+    }));
+});
